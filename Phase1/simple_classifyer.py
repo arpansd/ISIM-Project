74fea83505588c7a29febd77d286179ce3436c51
Meko27 <meihenkohsar@msn.com>
2019-12-28 17:55:44 +0100

    some progress

--- Phase1/simple_classifyer.py
+++ Phase1/simple_classifyer.py
@@ -11,6 +11,8 @@ from skimage.feature import hog
 import cv2
 from PIL import Image
 import itertools
+import pandas as pd
+import ntpath
 
 def chunked_iterable(iterable, size):
 # Auxiliary function to iterate over chunks
@@ -21,13 +23,17 @@ def chunked_iterable(iterable, size):
             break
         yield chunk
 
-
 def load_data_batch(datapath,batch_size = 20):
 # Auxiliary Function to load images batchwise from a specific path
-    valid_img_type = [".jpg"] # all images are type jpg
+    valid_img_type = '.jpg' # all images are type jpg
     img_hog = []
     # laod all image addrs into a list
-    img_addrs_list = glob.glob(datapath + '/*' + valid_img_type[0])
+    img_addrs_list = glob.glob(datapath + '/*' + valid_img_type)
+    img_ident_list = [None] * len(img_addrs_list) 
+    for i,addr in enumerate(img_addrs_list): 
+        img_ident = ntpath.basename(addr)
+        img_ident = img_ident[:-len(valid_img_type)]
+        img_ident_list[i] = img_ident # list of img names
     counter = 0
     for i in chunked_iterable(img_addrs_list,size=batch_size): # iterate over each batch
             counter +=1
@@ -35,12 +41,11 @@ def load_data_batch(datapath,batch_size = 20):
             for addr in img_addr_batch:
                 img = ski.imread(addr)
                 hog_vector = hog(img,orientations=9,pixels_per_cell=(64,64))
-                img_hog.append(hog_vector)
+                img_hog.append(hog_vector) # TODO allocate list, because appending is inefficient!
             print('processing batch {} of {}'.format(counter,len(img_addrs_list)//batch_size),'\n')
     img_hog = np.array(img_hog) # transform to np-array
-    return img_hog,img_addrs_list
+    return img_hog, img_ident_list, img_addrs_list
 
-''
 def normalize_by_length(feat_array,norm_order=1):
     # Auxiliary function to normalize feature vectors
     feat_array_norm = []
@@ -50,27 +55,65 @@ def normalize_by_length(feat_array,norm_order=1):
     feat_array_norm = np.array(feat_array_norm)   
     return feat_array_norm 
 
+def train_val_split(dataset,ident_dataset,label_train,label_val):
+    # Auxiliary Fct to split the data_set into train and validation data acc. to groundtruth
+    (_,dim_data) = dataset.shape
+    n_train = len(label_train)-1
+    n_val = len(label_val)-1
+    data_train = np.empty([n_train,dim_data])
+    idx_train = np.empty([n_train])
+    data_val = np.empty([n_val,dim_data])
+    idx_val = np.empty([n_val])
+
+    for idx,img_ident in enumerate(label_train['image']):
+        idx_train[idx] = ident_dataset.index(img_ident) 
+        data_train[idx] = dataset[idx_train]
+    for idx,img_ident in enumerate(label_val['image']):
+        idx_val[idx] = ident_dataset.index(img_ident)
+        data_val[idx] = dataset[idx_val]
+    
+    return data_train, data_val
+
+def sort_data(data,ident_data,ident_reference):
+    # Auxiliary Fct to sort data acc. to a sorted ident list
+    (_,dim_data) = data.shape
+    n_data_sorted = len(ident_reference)-1 # length without header
+    data_sorted = np.empty([n_data_sorted,dim_data])
+    idx_sorted = np.empty([n_data_sorted])
+    for i,ident in enumerate(ident_reference['image']):
+        idx_sorted[i] = ident_data.index(ident)
+        data_sorted[i] = data[idx_sorted[i]]
+
+    return data_sorted
+
 
 def main():
 
-    # define variables to extract features
+    # data paths and groundtruth
     path_train = '/Users/meko/Documents/Repos/ISIM-Project_local/data/ISIC_2019_Training_Input'
     path_test = '/Users/meko/Documents/Repos/ISIM-Project_local/data/ISIC_2019_Test_Input'
-    batch_size = 20
-    # Extraxt features
-    img_train_hog,img_train_addrs_list = load_data_batch(path_train,batch_size)
-    img_test_hog,img_test_addrs_list = load_data_batch(path_test,batch_size)
-    print('length of img_hog: ' , len(img_train_hog))
-    print('length of each hog_vector:' , len(img_train_hog[0]))
+    path_train_gt = '/Users/meko/Documents/Repos/ISIM-Project_local/data/groundtruth_train.csv'
+    path_val_gt = '/Users/meko/Documents/Repos/ISIM-Project_local/data/groundtruth_val.csv'
+    gt_train = pd.read_csv(path_train_gt,sep=',')
+    gt_val = pd.read_csv(path_val_gt,sep=',')
+    
+    # set variables
+    
+
+    # Extract features 
+    hog_feat_train,ident_train = load_data_batch(path_train, batch_size=20)
+    hog_feat_test,ident_train = load_data_batch(path_test, batch_size=20)
+    print('length of img_hog: ' , len(hog_feat_train))
+    print('length of each hog_vector:' , len(hog_feat_train[0]))
     
     # normalize feature vector
     norm_order = 1
-    img_train_hog_norm = normalize_by_length(img_train_hog,norm_order)
-    img_test_hog_norm = normalize_by_length(img_test_hog,norm_order)
+    hog_feat_train_norm = normalize_by_length(hog_feat_train,norm_order)
+    hog_feat_test_norm = normalize_by_length(hog_feat_test,norm_order)
 
-    # Split training data and validation data
-    
-
+    # Split into training data and validation data according to ground truth 
+    hog_feat_train_sorted = sort_data(hog_feat_train_norm,ident_train,gt_train)
+    hog_feat_val_sorted = sort_data(hog_feat_train_norm,ident_train,gt_val)
 
     # Train classifier
 
@@ -80,14 +123,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-        
-
-
-
-
 
